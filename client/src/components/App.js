4e061e8948e5c5c3c7618a12360e8e0813d5a6e2
Rendon De La Paz <rendondlp@gmail.com>
2019-05-09 15:51:33 -0700

    test commit

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <h1>Hello World</h1>
+        <h1>Family Recipes</h1>
       </Provider>
     )
   }
