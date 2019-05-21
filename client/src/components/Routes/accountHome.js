5d5a08b8c62bd1c1896214b65961c399e20db5cf
Rendon De La Paz <rendondlp@gmail.com>
2019-05-21 12:29:11 -0700

    added line through styling

--- client/src/components/Routes/accountHome.js
+++ client/src/components/Routes/accountHome.js
@@ -19,23 +19,23 @@ class AccountHome extends Component {
         <div className="canvas2">
        
 
-        <div className="accountNAV" id="left-panel">
-          <Link to="/user_recipes"><p className="user-recipes">My Recipes</p></Link>
-          <Link to="/user_groups"><p className="user-groups">My Groups</p></Link>
-          <Link to="user_messages"><p className="user-messages">My Messages</p></Link>
-          <Link to="/upload"><p className="upload">Upload Recipe</p></Link>
-          <Link to="/user_profile"><p className="user-profile">My Profile</p></Link>
+        <div className="left-panel">
+          <Link to="/user_recipes"><p className="leftP">My Recipes</p></Link>
+          <Link to="/user_groups"><p className="leftP">My Groups</p></Link>
+          <Link to="user_messages"><p className="leftP">My Messages</p></Link>
+          <Link to="/upload"><p className="leftP">Upload Recipe</p></Link>
+          <Link to="/user_profile"><p className="leftP">My Profile</p></Link>
         </div>
 
         <div className="accountNAV" id="mid-panel">
-          <Link to="/user_fav_recipes"><h1 className="title">My Favorite Recipes</h1></Link>
+          <Link to="/user_fav_recipes"><h2 className="title">My Favorite Recipes</h2></Link>
           <Link to="/usermade_recipebook"><p>Recipebook(playlist) 1</p></Link>
           <p>Recipebook(playlist) 2</p>
           <p>Recipebook(playlist) 3</p>
         </div>
 
           <div className="accountNAV" id="right-panel">
-            <Link to="/recent_updates"><h1 className="title">Recent Updates</h1></Link>
+            <Link to="/recent_updates"><h2 className="title">Recent Updates</h2></Link>
           </div>
 
         </div>
