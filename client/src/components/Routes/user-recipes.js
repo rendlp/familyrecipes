d9293617a936bc5be49abf09f00576de00120093
Rendon De La Paz <rendondlp@gmail.com>
2019-05-24 11:00:13 -0700

    merged

--- client/src/components/Routes/user-recipes.js
+++ client/src/components/Routes/user-recipes.js
@@ -34,7 +34,7 @@ const UserRecipe = (props) => {
         </div>
         <div className='userRecipeDiv'>
          <div className='recipeDiv'>
-        {props.userRecipes.map(recipe => (
+        {props.userRecipes.map((recipe, i) => (
           <Link className='recipeLink'key={'recipe'+i} to={'user_recipes/' + recipe.recipe_id}><p>{recipe.name == null ? "Unnamed Recipe" : recipe.name}</p></Link>
 
         ))}
