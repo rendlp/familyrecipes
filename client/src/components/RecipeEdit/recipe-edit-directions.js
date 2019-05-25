c15b26bc7593a39fad656e7c58d6b6aa92ec9704
Adam Horvath <adamhorvath108@gmail.com>
2019-05-24 17:15:08 -0700

    recipe edit page layout added

---
+++ client/src/components/RecipeEdit/recipe-edit-directions.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react'
+import useFormInput from '../hooks/useFormInput'
+
+const EditDirections = (props) => {
+
+    const [values, changeForm, resetForm] = useFormInput({...props.formData})
+
+    let manageFunc = props.manageForm
+
+    useEffect( () => {
+        manageFunc('directions', values)
+    }, [values])
+
+        return (
+            <form>
+                <div className="directionDiv">
+                    <label
+                        htmlFor='name'
+                        className="directions-text"></label>
+                    <h1>Directions</h1>
+            
+
+                    <textarea
+                        type="text"
+                        name="directions"
+                        id="directions"
+                        onChange= {changeForm}
+                        value={values.directions || ''}
+                    />
+                </div>
+            </form>
+        )
+
+}
+
+export default EditDirections
--- client/src/components/RecipeEdit/recipe-view-edit.js
+++ client/src/components/RecipeEdit/recipe-view-edit.js
@@ -4,72 +4,79 @@ import { useSelector } from 'react-redux'
 // import Footer from '../../footer'
 import  { Link } from 'react-router-dom'
 import { withRouter } from 'react-router-dom'
+import { AuthContext } from "../../lib/auth"
+import EditName from './recipe-edit-name'
+import EditPrep from './recipe-edit-prep'
+import EditDirections from './recipe-edit-directions'
+import EditIngredient from './recipe-edit-ingredients'
+import EditImageUpload from './recipe-edit-image-upload'
+// import { editRecipe } from '../../actions/actions'
 
 const RecipeViewEdit = props => {
 
 const recipeId = props.match.params.recipe_id
 
-// const { user } = useContext(AuthContext)
+const { user } = useContext(AuthContext)
 
-//   const forms = {
-//         username: {user},
-//         ingredient: {
-//           list: []
-//         },
-//         image: ""
-//   };
+  const forms = {
+        username: {user},
+        ingredient: {
+          list: []
+        },
+        image: ""
+  };
 
-//   function manageForm(ctx, payload) {
-//     // console.log('updating', ctx, payload);
-//     forms[ctx] = {...payload};
-// };
+  function manageForm(ctx, payload) {
+    // console.log('updating', ctx, payload);
+    forms[ctx] = {...payload};
+};
 
-// function addImageToForm(img) {
-//   forms.image = img
-// }
+function addImageToForm(img) {
+  forms.image = img
+}
 
 
-// // on form submit
-// function handleForm(e) {
-//     e.preventDefault();
-//     addRecipe(forms);
-//     props.history.push('/user_recipes');
-// };
+//on form submit
+function handleForm(e) {
+    e.preventDefault();
+    // addRecipe(forms);
+    props.history.push('/user_recipes');
+};
 
 return (
 
     <div className="uploadDiv">
     
-        {/* <Link to='/'><button className='abutton'>Back</button></Link>
+        <Link to='/'><button className='abutton'>Back</button></Link>
 
 
        <div id="name/prep">
 
-           <Name manageForm={manageForm} formData={forms.RecipeName} />
-           <Prep manageForm={manageForm} formData={forms.PrepTime} />
+           <EditName manageForm={manageForm} formData={forms.RecipeName} />
+           <EditPrep manageForm={manageForm} formData={forms.PrepTime} />
        </div>
 
 
        <div className="ingredients">
 
            <h1 className=''>Ingredients</h1>
-           <Ingredient manageForm={manageForm} formData={forms.ingredient} />
+           <EditIngredient manageForm={manageForm} formData={forms.ingredient} />
           
        </div>
 
        <div id="text-directions">
 
-           <Directions manageForm={manageForm} formData={forms.Directions} />
+           <EditDirections manageForm={manageForm} formData={forms.Directions} />
        </div>
        <div id="image-upload">
            <h1>Image</h1>
-           <ImageUpload addImageToForm={addImageToForm} manageForm={manageForm} formData={forms.image} />
+           <EditImageUpload addImageToForm={addImageToForm} manageForm={manageForm} formData={forms.image} />
            <form onSubmit={handleForm}>
 
         <button className='abutton'>Submit</button>
        </form>
 
-       </div> */}
+       </div>
 
 
     </div>
