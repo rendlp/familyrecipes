3cded0f7f0697df0029808b3a56e2690a9ae275b
Adam Horvath <adamhorvath108@gmail.com>
2019-05-25 18:28:21 -0700

    edit recipe page content created and responsive

--- client/src/components/hooks/useFormInput.js
+++ client/src/components/hooks/useFormInput.js
@@ -8,25 +8,7 @@ export default function useFormInput(formState={}) {
         modifyForm(values => ({...values, [e.target.name]: e.target.value}));
     };
 
-    // const resetForm = (e) => {
-    //     e.preventDefault();
-    //
-    //     for (var key in formValues) {
-    //         formValues[key] = '';
-    //     }
-    //
-    //     modifyForm({...formValues});
-    // };
+    
 
     return [formValues, setForm];
 }
-
-// const [values, modifyForm] = useState({});
-//
-// const changeForm = (e) => {
-//     // values[e.target.name] = e.target.value;
-//     // console.log('values =>', values);
-//     // modifyForm(values);
-//     e.persist();
-//     modifyForm(values => ({...values, [e.target.name]: e.target.value}));
-// };
