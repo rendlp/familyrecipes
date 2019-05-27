844097a748ef4be45e54fd7e1f86d05b942443f6
Rendon De La Paz <rendondlp@gmail.com>
2019-05-27 15:15:07 -0700

    added picture thumbnails

--- client/src/components/Routes/RecipeBookView.js
+++ client/src/components/Routes/RecipeBookView.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
 import {Link} from 'react-router-dom'
 import Header from '../header'
 import Footer from '../footer'
@@ -13,6 +14,8 @@ const RecipeBookView = (props) => {
   // const { user } = useContext(AuthContext)
 
   const recipebookID = props.match.params.recipebook_id
+
+  const userRecipes = useSelector(appstate => appstate.addedRecipesInsideRecipebooks)
 
   useEffect( () => {
       getRecipesWithinRecipebooks(recipebookID)
@@ -38,6 +41,7 @@ console.log(props.addedRecipesInsideRecipebooks)
 
         <Link className='recipeLink' to={`/user_fav_recipes/recipebook/${recipebookID}/${recipe.recipe_id}`}>
         <div className='recipeListP'>{recipe.recipe_name == null ? "Unnamed Recipe" : recipe.recipe_name}
+        <img src={recipe.imgURL || 'https://via.placeholder.com/200'} alt='' />
         </div>
         </Link>
 
